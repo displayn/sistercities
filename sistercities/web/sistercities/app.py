ac7aad3d7c8599283f4c28b775efbbf944a747c5
Tobias Scholz <dev@display.name>
2017-01-10 00:08:07 +0100

    add cache an opacity effect

--- sistercities/web/sistercities/app.py
+++ sistercities/web/sistercities/app.py
@@ -2,12 +2,16 @@
 from flask import Flask
 from flask import render_template
 import sistercities.web.sistercities.sister_graph as sister_graph
+from flask_cache import Cache
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = '86133838741634802826072472476'
+
+cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 from networkx.readwrite import json_graph
 import json
-import os
-
 
 def read_json_file(filename: object) -> object:
     # from http://stackoverflow.com/a/34665365
@@ -21,14 +25,15 @@ def read_json_file(filename: object) -> object:
 
 
 @app.route('/')
+@cache.cached(timeout=500)
 def table():
     datajson = read_json_file(app.open_resource('wikipedia.json'))
     wikijson = read_json_file(app.open_resource('wikidata.json'))
-
     datasource = sister_graph.get(wikijson, datajson)
 
     return render_template('table.html', data=datasource)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #toolbar = DebugToolbarExtension(app)
+    app.run()
