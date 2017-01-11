96994460bce108582feeff495ec9bb77f409d007
Tobias Scholz <dev@display.name>
2017-01-11 18:37:44 +0100

    add stats

--- sistercities/web/sistercities/app.py
+++ sistercities/web/sistercities/app.py
@@ -4,19 +4,20 @@ from flask import render_template
 import sistercities.web.sistercities.sister_graph as sister_graph
 from flask_cache import Cache
 from flask_debugtoolbar import DebugToolbarExtension
+import os.path, datetime
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = '86133838741634802826072472476'
 
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 from networkx.readwrite import json_graph
 import json
+
 
 def read_json_file(filename: object) -> object:
     # from http://stackoverflow.com/a/34665365
     """
-
     :type filename: object
     """
     with open(filename.name) as f:
@@ -24,16 +25,22 @@ def read_json_file(filename: object) -> object:
     return json_graph.node_link_graph(js_graph)
 
 
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
+
 @app.route('/')
 @cache.cached(timeout=500)
 def table():
+    d = modification_date(app.open_resource('wikipedia.json').name)
     datajson = read_json_file(app.open_resource('wikipedia.json'))
     wikijson = read_json_file(app.open_resource('wikidata.json'))
     datasource = sister_graph.get(wikijson, datajson)
 
-    return render_template('table.html', data=datasource)
+    return render_template('table.html', data=datasource, stats=d)
 
 
 if __name__ == '__main__':
-    #toolbar = DebugToolbarExtension(app)
+    # toolbar = DebugToolbarExtension(app)
     app.run()
